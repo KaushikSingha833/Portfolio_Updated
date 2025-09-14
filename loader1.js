4dce103fbb924ef598f7640469985d405328ebc7
KaushikSingha833 <kaushiksingha081@gmail.com>
2025-09-14 19:29:02 +0530

    Add files via upload

---
+++ loader1.js
@@ -0,0 +1,24 @@
+window.addEventListener('pageshow', function(event) {
+    const loader = document.getElementById('loading-overlay');
+    
+    // Hide the loader immediately
+    loader.style.transition = 'opacity 0.5s ease';
+    loader.style.opacity = '0';
+    setTimeout(() => {
+      loader.style.display = 'none';
+    }, 500);
+  });
+
+  
+  document.querySelectorAll('a[href^="http"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const loader = document.getElementById('loading-overlay');
+      loader.style.display = 'flex';
+      loader.style.opacity = '1'; 
+      const href = this.href;
+      setTimeout(() => {
+        window.location.href = href;
+      }, 1000);
+    });
+  });
